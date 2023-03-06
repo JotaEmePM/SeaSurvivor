eace329b35f4244c265008c9e5fac1dffc44183c
JotaEmePM <perezmjosem@gmail.com>
2023-03-06 17:52:42 -0300

    Se añade scene World

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -6,6 +6,8 @@ const FRICTION = 40.0
 
 var DIRECTION = "DOWN"
 
+var skin_number = 1
+
 func _ready():
 	$AnimationPlayer.play()
 	$AnimationPlayer.current_animation = "idle_down"
@@ -13,6 +15,28 @@ func _ready():
 func _physics_process(delta):
 	input_movement(delta)
 	sprite_animation_selector()
+	change_skin()
+
+func change_skin():
+	var l_pressed = Input.is_action_pressed("Change_Skin_L")
+	var r_pressed = Input.is_action_pressed("Change_Skin_R")
+	
+	if $Timer.is_stopped():
+		if(l_pressed):
+			$Timer.start()
+			if(skin_number == 1):		
+				skin_number = 8
+			else: 
+				skin_number -= 1
+				
+		if(r_pressed):
+			$Timer.start()
+			if(skin_number == 8):		
+				skin_number = 1
+			else: 
+				skin_number += 1
+			
+	$Sprite2D.texture = load("res://assets/sprites/character/characters/char" + str(skin_number) + ".png")
 
 func sprite_animation_selector():
 	match DIRECTION:
@@ -54,7 +78,5 @@ func input_movement(delta):
 		DIRECTION = "WALK_UP"	
 	if velocity.y == 0 and velocity.x == 0:
 		DIRECTION = DIRECTION.replace("WALK","IDLE")
-	
-	
 	
 	move_and_slide()
